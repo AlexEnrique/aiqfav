3ffab08035b6e282fc674bc89f2739e6eb2f96e4
AlexEnrique <alexenriquecrispim@hotmail.com>
2025-09-04 16:37:33 -0300

    refact: refactored customer v1 route and instantiation of FastAPI app

--- aiqfav/api/app.py
+++ aiqfav/api/app.py
@@ -1,42 +1,11 @@
-from typing import Annotated
+from fastapi import FastAPI
 
-from fastapi import Depends, FastAPI, HTTPException
+from .routes.customers_v1 import router as customers_router_v1
 
-from aiqfav.domain.customer import CustomerCreate
-from aiqfav.services.customer import CustomerService
-from aiqfav.services.customer.exceptions import EmailAlreadyExists
-from aiqfav.utils.api_errors import ErrorCodes, get_error_response
-
-from .dependencies import get_customer_service
-
-app = FastAPI()
+__all__ = ['create_app']
 
 
-@app.get('/customers')
-async def list_customers(
-    customer_service: Annotated[
-        CustomerService, Depends(get_customer_service)
-    ],
-):
-    """Endpoint para listar todos os clientes"""
-    return await customer_service.list_customers()
-
-
-@app.post('/customers')
-async def create_customer(
-    customer_service: Annotated[
-        CustomerService, Depends(get_customer_service)
-    ],
-    customer: CustomerCreate,
-):
-    """Endpoint para criar um novo cliente"""
-    try:
-        return await customer_service.create_customer(customer)
-    except EmailAlreadyExists:
-        raise HTTPException(
-            status_code=409,
-            detail=get_error_response(
-                error_code=ErrorCodes.EMAIL_ALREADY_EXISTS,
-                message='Já existe um cliente com este e-mail',
-            ),
-        )
+def create_app() -> FastAPI:
+    app = FastAPI()
+    app.include_router(customers_router_v1, prefix='/v1')
+    return app
