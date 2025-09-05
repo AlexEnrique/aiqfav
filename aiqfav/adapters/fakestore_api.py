26fdac5f39280a1dbcfc0bd52e314eeb5ea7cbb6
AlexEnrique <alexenriquecrispim@hotmail.com>
2025-09-05 07:52:11 -0300

    feat: added cache for products api

--- aiqfav/adapters/fakestore_api.py
+++ aiqfav/adapters/fakestore_api.py
@@ -1,7 +1,11 @@
 import asyncio
+import json
+import logging
 from typing import Iterable
 
 import httpx
+import redis.asyncio as redis
+from environs import Env
 
 from aiqfav.domain.product import ProductPublic
 from aiqfav.utils.httpx import raise_for_status
@@ -11,6 +15,12 @@ from .exceptions import StoreApiNotFoundError, StoreApiUnexpectedResponseError
 
 __all__ = ['FakeStoreApi']
 
+env = Env()
+env.read_env()
+
+
+CACHE_EXPIRATION = 60 * 60  # 1 hour
+
 
 class FakeStoreApi(StoreApiAdapter):
     def __init__(self, base_url: str):
@@ -18,11 +28,27 @@ class FakeStoreApi(StoreApiAdapter):
             'base_url must be a non-empty string'
         )
         self.base_url = base_url.rstrip('/')
+        self.redis = redis.Redis(
+            host=env('REDIS_HOST'),
+            port=env.int('REDIS_PORT'),
+            db=env.int('REDIS_DB'),
+        )
 
     async def list_products(self) -> list[ProductPublic]:
         """List products from the store API"""
+        logging.info('Listing products from the store API')
 
         async with httpx.AsyncClient() as session:
+            cached_data = await self.redis.get('products')
+            if cached_data:
+                logging.debug('Cache hit for products')
+                return [
+                    self._validate_product(product)
+                    for product in json.loads(cached_data)
+                ]
+            else:
+                logging.debug('Cache miss for products')
+
             response = await session.get(f'{self.base_url}/products')
 
             data = raise_for_status(
@@ -31,14 +57,36 @@ class FakeStoreApi(StoreApiAdapter):
             )
 
             assert isinstance(data, list)
+            await self.redis.set(
+                'products', json.dumps(data), ex=CACHE_EXPIRATION
+            )
+
+            pipeline = self.redis.pipeline()
+            for product in data:
+                pipeline.set(
+                    f'product:{product["id"]}',
+                    json.dumps(product),
+                    ex=CACHE_EXPIRATION,
+                )
+            await pipeline.execute()
+
             return [self._validate_product(product) for product in data]
 
     async def get_product(self, product_id: int) -> ProductPublic:
         """Get a product from the store API"""
+        logging.info('Getting product %s from the store API', product_id)
+
         async with httpx.AsyncClient() as session:
             response = await session.get(
                 f'{self.base_url}/products/{product_id}'
             )
+
+            cached_data = await self.redis.get(f'product:{product_id}')
+            if cached_data:
+                logging.debug('Cache hit for product %s', product_id)
+                return self._validate_product(json.loads(cached_data))
+            else:
+                logging.debug('Cache miss for product %s', product_id)
 
             if response.status_code == 404:
                 raise StoreApiNotFoundError(
@@ -51,12 +99,21 @@ class FakeStoreApi(StoreApiAdapter):
             )
 
             assert isinstance(data, dict)
+            await self.redis.set(
+                f'product:{product_id}', json.dumps(data), ex=CACHE_EXPIRATION
+            )
+
             return self._validate_product(data)
 
     async def get_products_in_batch(
         self, product_ids: Iterable[int]
     ) -> list[ProductPublic]:
         """Get products in batch from the store API"""
+        logging.info(
+            'Getting products in batch %s from the store API', product_ids
+        )
+
+        # Usando asyncio.gather para executar as chamadas de forma concorrente
         return await asyncio.gather(
             *[self.get_product(product_id) for product_id in product_ids]
         )
